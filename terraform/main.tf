408519eeaf574b098317590ee03786662ce7b70f
YuvalGlizerin <yuval.glizerin@postrix.io>
2024-02-18 01:46:39 +0200

    don't use variables

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,8 +11,8 @@ terraform {
 }
 
 provider "google" {
-  project = var.project
-  region  = var.region
+  project = "postrix"
+  region  = "us-central1"
 }
 
 resource "google_compute_network" "vpc_network" {
