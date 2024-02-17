47bd63b0e882d06d1477d51eb9807bbcd6a8c158
YuvalGlizerin <yuval.glizerin@postrix.io>
2024-02-18 01:23:04 +0200

    updated required version

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,14 +1,13 @@
 # Requires authentication: gcloud auth application-default login
 
 terraform {
-  required_version = "= 1.7.3"
-
   required_providers {
     google = {
       source = "hashicorp/google"
       version = "5.16.0"
     }
   }
+  required_version = "= 1.7.3"
 }
 
 provider "google" {
