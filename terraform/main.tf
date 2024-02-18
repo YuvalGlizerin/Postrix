036e083982ebbca30a13b3bbd50bf09ca9176f8e
YuvalGlizerin <yuval.glizerin@postrix.io>
2024-02-19 00:24:57 +0200

    Remove all resources

--- terraform/main.tf
+++ terraform/main.tf
@@ -15,24 +15,24 @@ provider "google" {
   zone    = "us-central1-a"
 }
 
-resource "google_compute_network" "vpc_network" {
-  name = "terraform-network"
-}
+# resource "google_compute_network" "vpc_network" {
+#   name = "terraform-network-2"
+# }
 
-resource "google_compute_instance" "vm_instance" {
-  name         = "terraform-instance"
-  machine_type = "f1-micro"
-  tags         = ["web", "dev"]
+# resource "google_compute_instance" "vm_instance" {
+#   name         = "terraform-instance-2"
+#   machine_type = "f1-micro"
+#   tags         = ["web", "dev"]
 
-  boot_disk {
-    initialize_params {
-      image = "cos-cloud/cos-stable"
-    }
-  }
+#   boot_disk {
+#     initialize_params {
+#       image = "cos-cloud/cos-stable"
+#     }
+#   }
 
-  network_interface {
-    network = google_compute_network.vpc_network.name
-    access_config {
-    }
-  }
-}
+#   network_interface {
+#     network = google_compute_network.vpc_network.name
+#     access_config {
+#     }
+#   }
+# }
