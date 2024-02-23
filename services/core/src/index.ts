60aba213c822e6e2843f29dd6b359f3cef15bc11
YuvalGlizerin <yuval.glizerin@postrix.io>
2024-02-23 19:52:02 +0200

    Fixed dangling test

--- services/core/src/index.ts
+++ services/core/src/index.ts
@@ -9,8 +9,9 @@ app.get('/', (req: Request, res: Response) => {
 });
 
 const PORT = 8000;
-app.listen(PORT, () => {
+
+const server = app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
 
-export const core = app;
+export default server;
--- services/core/tests/test.test.ts
+++ services/core/tests/test.test.ts
@@ -1,13 +1,20 @@
 import supertest from 'supertest';
-import { core } from '../src/index'; // Replace './index' with the actual path to your Cloud Function file
+import core from '../src/index'; // Replace './index' with the actual path to your Cloud Function file
 
 // Since myFunction is an Express app, we can use it directly with supertest
 const request = supertest(core);
 
 describe('GET /', () => {
+  
+
   it('responds with Hello, World from Google Cloud Functions!', async () => {
     const response = await request.get('/');
     expect(response.status).toBe(200);
     expect(response.text).toBe('Hello, World from Google Cloud Functions!');
   });
+});
+
+afterAll(done => {
+  core.close();
+  done();
 });
