bc2d93d44a18a0432bd0b54c6d6fb77e71c6765c
YuvalGlizerin <yuval.glizerin@postrix.io>
2024-02-23 20:02:14 +0200

    updated main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,29 +10,7 @@ terraform {
 }
 
 provider "google" {
-  project = "postrix"
-  region  = "us-central1"
-  zone    = "us-central1-a"
-}
-
-resource "google_compute_network" "vpc_network" {
-  name = "terraform-network-2"
-}
-
-resource "google_compute_instance" "vm_instance" {
-  name         = "terraform-instance-2"
-  machine_type = "f1-micro"
-  tags         = ["web", "dev"]
-
-  boot_disk {
-    initialize_params {
-      image = "cos-cloud/cos-stable"
-    }
-  }
-
-  network_interface {
-    network = google_compute_network.vpc_network.name
-    access_config {
-    }
-  }
+  project = var.project
+  region  = var.region
+  zone    = var.zone
 }
