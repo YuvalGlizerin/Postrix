c0479f63375b5d8d809087155a91b08807febd1c
YuvalGlizerin <yuval.glizerin@postrix.io>
2024-02-25 23:28:50 +0200

    Add core service to terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -15,27 +15,27 @@ provider "google" {
   zone    = var.zone
 }
 
-resource "google_artifact_registry_repository" "my_repository" {
+resource "google_artifact_registry_repository" "artifact_registry_production" {
   provider      = google
   location      = var.region
   repository_id = "production-docker"
   format        = "DOCKER"
 }
 
-# resource "google_cloud_run_service" "default" {
-#   name     = "express-app-service"
-#   location = "us-central1"
+resource "google_cloud_run_service" "core" {
+  name     = "core-service"
+  location = "${var.region}"
 
-#   template {
-#     spec {
-#       containers {
-#         image = "gcr.io/${var.project}/express-app:latest"
-#       }
-#     }
-#   }
+  template {
+    spec {
+      containers {
+        image = "${var.region}-docker.pkg.dev/${var.project}/${google_artifact_registry_repository.artifact_registry_production.repository_id}/core:latest"
+      }
+    }
+  }
 
-#   traffic {
-#     percent         = 100
-#     latest_revision = true
-#   }
-# }
+  traffic {
+    percent         = 100
+    latest_revision = true
+  }
+}
