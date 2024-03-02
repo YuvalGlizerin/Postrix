4e93f2de0acb1cbedcd0864d1fd48ea6f5d28800
YuvalGlizerin <yuval.glizerin@postrix.io>
2024-03-02 13:36:35 +0200

    Added provider

--- terraform/modules/core/main.tf
+++ terraform/modules/core/main.tf
@@ -41,6 +41,7 @@ resource "google_cloud_run_service" "core" {
 }
 
 resource "google_cloud_run_service_iam_member" "public_invoker" {
+  provider = google
   project  = var.project
   service  = google_cloud_run_service.core.name
   location = google_cloud_run_service.core.location
