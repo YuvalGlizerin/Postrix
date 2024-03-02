23d56fc4e74209a57c162d84249f23bdc8b65255
YuvalGlizerin <yuval.glizerin@postrix.io>
2024-03-02 02:06:05 +0200

    log the env

--- services/core/src/index.ts
+++ services/core/src/index.ts
@@ -12,7 +12,7 @@ app.get('/', (req: Request, res: Response) => {
 const PORT = process.env.PORT;
 
 const server = app.listen(PORT, () => {
-  console.log(`Server is running: http://localhost:${PORT}`);
+  console.log(`Server is running on ${process.env.ENV}: http://localhost:${PORT}`);
 });
 
 export default server;
