9ea5acaa8985d64cc22edd5a6550801288febd63
YuvalGlizerin <yuval.glizerin@postrix.io>
2024-03-09 18:32:58 +0200

    Fix terraform plan

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,7 +1,3 @@
-output "production_url" {
-  value = module.postrix_production.service_url
-}
-
-output "sandbox_url" {
-  value = module.postrix_sandbox.service_url
+output "core_url" {
+  value = module.core.service_url
 }
