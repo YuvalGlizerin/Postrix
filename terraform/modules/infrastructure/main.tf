81ffb70fb5dde30a457018af175b4df07ea81759
YuvalGlizerin <yuval.glizerin@postrix.io>
2024-03-11 00:10:17 +0200

    Added cleanup policies to artifact registry

--- terraform/modules/infrastructure/main.tf
+++ terraform/modules/infrastructure/main.tf
@@ -17,5 +17,34 @@ resource "google_artifact_registry_repository" "artifact_registry" {
   location      = var.region
   repository_id = var.artifactory_repository_id
   format        = "DOCKER"
+
+  cleanup_policies {
+    id          = "keep-prod-release"
+    action      = "KEEP"
+
+    condition {
+      tag_state = "TAGGED"
+      tag_prefixes = ["prod"]
+    }
+  }
+
+  cleanup_policies {
+    id          = "keep-minimum-versions"
+    action      = "KEEP"
+
+    most_recent_versions {
+      keep_count = 5
+    }
+  }
+
+  cleanup_policies {
+    id          = "gc-untagged"
+    action      = "DELETE"
+
+    condition {
+      tag_state = "UNTAGGED"
+      older_than = "2592000s" # 30 days
+    }
+  }
 }
 
