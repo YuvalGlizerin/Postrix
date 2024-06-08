9090379ac5fe975f331f1c764cc68477aa410a2e
YuvalGlizerin <yuval.glizerin@postrix.io>
2024-06-08 21:41:14 +0300

    Added terraform comment pr typescript custom action

---
+++ .github/actions/terraform-comment-pr/src/index.ts
@@ -0,0 +1,49 @@
+import * as core from '@actions/core';
+import * as github from '@actions/github';
+import { Context } from '@actions/github/lib/context';
+
+async function run() {
+    try {
+        const token = core.getInput('github_token');
+        const add = core.getInput('add');
+        const change = core.getInput('change');
+        const destroy = core.getInput('destroy');
+        const run_link = core.getInput('run_link');
+        const octokit = github.getOctokit(token);
+
+        const context: Context = github.context;
+        const { data: comments } = await octokit.rest.issues.listComments({
+            owner: context.repo.owner,
+            repo: context.repo.repo,
+            issue_number: context.issue.number,
+        });
+        const botComment = comments.find(comment => {
+            return comment?.user?.type === 'Bot' && comment?.body?.includes('Terraform Cloud Plan Output')
+        });
+
+        const output = `#### Terraform Cloud Plan Output
+            \`\`\`
+            Plan: ${{ add }} to add, ${{ change }} to change, ${{ destroy }} to destroy.
+            \`\`\`
+            [Terraform Cloud Plan](${{ run_link }})
+            `;
+            
+        if (botComment) {
+            octokit.rest.issues.deleteComment({
+              owner: context.repo.owner,
+              repo: context.repo.repo,
+              comment_id: botComment.id,
+            });
+        }
+        octokit.rest.issues.createComment({
+            issue_number: context.issue.number,
+            owner: context.repo.owner,
+            repo: context.repo.repo,
+            body: output
+        });
+    } catch (error) {
+        core.setFailed(<Error>error);
+    }
+}
+
+run();
