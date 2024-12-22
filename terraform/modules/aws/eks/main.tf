6b0c209c04276da87748ca5818a26c50cdc1ccc2
yuvalglizerin-eb <116079286+yuvalglizerin-eb@users.noreply.github.com>
2024-12-22 16:47:18 +0200

    Add dns management

--- terraform/modules/aws/eks/main.tf
+++ terraform/modules/aws/eks/main.tf
@@ -20,6 +20,10 @@ resource "aws_eks_cluster" "postrix" {
   vpc_config {
     subnet_ids = var.subnet_ids
   }
+
+  tags = {
+    "alpha.eksctl.io/cluster-oidc-enabled" = "true"
+  }
 }
 
 // Managed node group for the EKS cluster, specifying instance type and scaling
@@ -118,7 +122,7 @@ resource "aws_iam_policy" "external_dns" {
           "route53:ChangeResourceRecordSets"
         ],
         Resource = [
-          "arn:aws:route53:::hostedzone/"
+          "arn:aws:route53:::hostedzone/*"
         ]
       },
       {
@@ -136,16 +140,140 @@ resource "aws_iam_policy" "external_dns" {
   })
 }
 
-// Attach the External DNS policy to the node role
-resource "aws_iam_role_policy_attachment" "attach_external_dns_policy" {
-  policy_arn = aws_iam_policy.external_dns.arn
-  role       = aws_iam_role.eks_node.name
-}
-
-// Create an OpenID Connect provider for the EKS cluster
-// This is necessary so that iam roles can be assumed from the EKS cluster
 resource "aws_iam_openid_connect_provider" "eks" {
   client_id_list  = ["sts.amazonaws.com"]
   thumbprint_list = [var.oidc_thumbprint]
-  url             = "https://oidc.eks.${var.region}.amazonaws.com/id/${aws_eks_cluster.postrix.id}"
+  url             = "https://oidc.eks.us-east-1.amazonaws.com/id/27822741B2F3481942B42867BB3425A5"
+
+  tags = {
+    "alpha.eksctl.io/cluster-name"   = "postrix"
+    "alpha.eksctl.io/eksctl-version" = "0.198.0"
+  }
 }
+
+# // IAM policy for AWS Load Balancer Controller
+# resource "aws_iam_policy" "aws_load_balancer_controller" {
+#   name        = "AWSLoadBalancerControllerIAMPolicy"
+#   description = "Policy for AWS Load Balancer Controller"
+
+#   policy = jsonencode({
+#     "Version": "2012-10-17",
+#     "Statement": [
+#       {
+#         "Effect": "Allow",
+#         "Action": [
+#           "iam:CreateServiceLinkedRole",
+#           "ec2:DescribeAccountAttributes",
+#           "ec2:DescribeAddresses",
+#           "ec2:DescribeAvailabilityZones",
+#           "ec2:DescribeInternetGateways",
+#           "ec2:DescribeVpcs",
+#           "ec2:DescribeSubnets",
+#           "ec2:DescribeSecurityGroups",
+#           "ec2:DescribeInstances",
+#           "ec2:DescribeNetworkInterfaces",
+#           "ec2:DescribeTags",
+#           "ec2:GetCoipPoolUsage",
+#           "ec2:DescribeCoipPools",
+#           "elasticloadbalancing:DescribeLoadBalancers",
+#           "elasticloadbalancing:DescribeLoadBalancerAttributes",
+#           "elasticloadbalancing:DescribeListeners",
+#           "elasticloadbalancing:DescribeListenerCertificates",
+#           "elasticloadbalancing:DescribeSSLPolicies",
+#           "elasticloadbalancing:DescribeRules",
+#           "elasticloadbalancing:DescribeTargetGroups",
+#           "elasticloadbalancing:DescribeTargetGroupAttributes",
+#           "elasticloadbalancing:DescribeTargetHealth",
+#           "elasticloadbalancing:DescribeTags"
+#         ],
+#         "Resource": "*"
+#       },
+#       {
+#         "Effect": "Allow",
+#         "Action": [
+#           "cognito-idp:DescribeUserPoolClient",
+#           "acm:ListCertificates",
+#           "acm:DescribeCertificate",
+#           "iam:ListServerCertificates",
+#           "iam:GetServerCertificate",
+#           "waf-regional:GetWebACL",
+#           "waf-regional:GetWebACLForResource",
+#           "waf-regional:AssociateWebACL",
+#           "waf-regional:DisassociateWebACL",
+#           "wafv2:GetWebACL",
+#           "wafv2:GetWebACLForResource",
+#           "wafv2:AssociateWebACL",
+#           "wafv2:DisassociateWebACL",
+#           "shield:GetSubscriptionState",
+#           "shield:DescribeProtection",
+#           "shield:CreateProtection",
+#           "shield:DeleteProtection"
+#         ],
+#         "Resource": "*"
+#       },
+#       {
+#         "Effect": "Allow",
+#         "Action": [
+#           "ec2:AuthorizeSecurityGroupIngress",
+#           "ec2:RevokeSecurityGroupIngress"
+#         ],
+#         "Resource": "*"
+#       },
+#       {
+#         "Effect": "Allow",
+#         "Action": [
+#           "elasticloadbalancing:CreateListener",
+#           "elasticloadbalancing:DeleteListener",
+#           "elasticloadbalancing:CreateRule",
+#           "elasticloadbalancing:DeleteRule",
+#           "elasticloadbalancing:AddTags",
+#           "elasticloadbalancing:RemoveTags",
+#           "elasticloadbalancing:ModifyLoadBalancerAttributes",
+#           "elasticloadbalancing:SetIpAddressType",
+#           "elasticloadbalancing:SetSecurityGroups",
+#           "elasticloadbalancing:SetSubnets",
+#           "elasticloadbalancing:DeleteLoadBalancer",
+#           "elasticloadbalancing:ModifyTargetGroup",
+#           "elasticloadbalancing:ModifyTargetGroupAttributes",
+#           "elasticloadbalancing:DeleteTargetGroup",
+#           "elasticloadbalancing:RegisterTargets",
+#           "elasticloadbalancing:DeregisterTargets",
+#           "elasticloadbalancing:SetWebAcl",
+#           "elasticloadbalancing:ModifyListener",
+#           "elasticloadbalancing:AddListenerCertificates",
+#           "elasticloadbalancing:RemoveListenerCertificates",
+#           "elasticloadbalancing:ModifyRule"
+#         ],
+#         "Resource": "*"
+#       }
+#     ]
+#   })
+# }
+
+# resource "aws_iam_role" "aws_load_balancer_controller" {
+#   name = "aws-load-balancer-controller"
+
+#   assume_role_policy = jsonencode({
+#     Version = "2012-10-17"
+#     Statement = [
+#       {
+#         Action = "sts:AssumeRoleWithWebIdentity"
+#         Effect = "Allow"
+#         Principal = {
+#           Federated = aws_iam_openid_connect_provider.eks.arn
+#         }
+#         Condition = {
+#           StringEquals = {
+#             "${replace(aws_iam_openid_connect_provider.eks.url, "https://", "")}:sub": "system:serviceaccount:kube-system:aws-load-balancer-controller"
+#           }
+#         }
+#       }
+#     ]
+#   })
+# }
+
+# resource "aws_iam_role_policy_attachment" "aws_load_balancer_controller" {
+#   policy_arn = aws_iam_policy.aws_load_balancer_controller.arn
+#   role       = aws_iam_role.aws_load_balancer_controller.name
+# }
+
--- terraform/modules/aws/route53/main.tf
+++ terraform/modules/aws/route53/main.tf
@@ -7,8 +7,25 @@ variable "domain" {
 
 resource "aws_route53domains_registered_domain" "domain" {
   domain_name = var.domain
+
+  name_server {
+    name = "ns-583.awsdns-08.net"
+  }
+  name_server {
+    name = "ns-330.awsdns-41.com"
+  }
+  name_server {
+    name = "ns-1619.awsdns-10.co.uk"
+  }
+  name_server {
+    name = "ns-1261.awsdns-29.org"
+  }
 }
 
 resource "aws_route53_zone" "zone" {
   name = var.domain
+}
+
+output "zone_id" {
+  value = aws_route53_zone.zone.zone_id
 }
