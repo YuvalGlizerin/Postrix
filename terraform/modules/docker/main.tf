b7486afdf50576cc9220691814ce529927204c5a
yuvalglizerin-eb <116079286+yuvalglizerin-eb@users.noreply.github.com>
2024-12-30 00:59:51 +0200

    Added comment regarding PAT token

--- terraform/modules/docker/main.tf
+++ terraform/modules/docker/main.tf
@@ -1,18 +1,15 @@
-# resource "docker_access_token" "postrix" {
-#   token_label = "Postrix Access Token"
-#   scopes      = ["repo:read", "repo:write", "repo:delete"]
-# }
+# The access token is created manually in Docker Hub, not managed by Terraform
 
-# resource "docker_hub_repository" "postrix" {
-#   name        = "postrix"
-#   namespace   = "yuvadius"
-#   description = "Postrix Docker repository"
-#   private     = false
-# }
+resource "docker_hub_repository" "postrix" {
+  name        = "postrix"
+  namespace   = "yuvadius"
+  description = "Postrix Docker repository"
+  private     = false
+}
 
-# resource "docker_hub_repository" "postrix-dev" {
-#   name        = "postrix-dev"
-#   namespace   = "yuvadius"
-#   description = "Postrix Dev Docker repository"
-#   private     = false
-# }
+resource "docker_hub_repository" "postrix-dev" {
+  name        = "postrix-dev"
+  namespace   = "yuvadius"
+  description = "Postrix Dev Docker repository"
+  private     = false
+}
