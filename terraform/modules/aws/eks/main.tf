3c9c6a7edb4ae675857f287e084acd1d20aad504
yuvalglizerin-eb <116079286+yuvalglizerin-eb@users.noreply.github.com>
2024-12-29 17:50:25 +0200

    Added postgress to the cluster

--- terraform/modules/aws/eks/main.tf
+++ terraform/modules/aws/eks/main.tf
@@ -150,3 +150,40 @@ resource "aws_iam_openid_connect_provider" "eks" {
     "alpha.eksctl.io/eksctl-version" = "0.198.0"
   }
 }
+
+# Create IAM role for EBS CSI driver
+resource "aws_iam_role" "ebs_csi_driver" {
+  name = "${var.cluster_name}-ebs-csi-driver"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Federated = aws_iam_openid_connect_provider.eks.arn
+        }
+        Action = "sts:AssumeRoleWithWebIdentity"
+        Condition = {
+          StringEquals = {
+            "${replace(aws_iam_openid_connect_provider.eks.url, "https://", "")}:sub": "system:serviceaccount:kube-system:ebs-csi-controller-sa"
+          }
+        }
+      }
+    ]
+  })
+}
+
+# Attach required AWS managed policy for EBS CSI driver
+resource "aws_iam_role_policy_attachment" "ebs_csi_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
+  role       = aws_iam_role.ebs_csi_driver.name
+}
+
+# Install EBS CSI driver add-on
+resource "aws_eks_addon" "ebs_csi" {
+  cluster_name = aws_eks_cluster.postrix.name
+  addon_name   = "aws-ebs-csi-driver"
+
+  service_account_role_arn = aws_iam_role.ebs_csi_driver.arn
+}
