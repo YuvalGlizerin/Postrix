3c60c22301e688232189cae490fdcfee6c0df3f5
yuvalglizerin-eb <116079286+yuvalglizerin-eb@users.noreply.github.com>
2025-01-01 22:40:56 +0200

    Set up app to be hosted on dev and prod

--- terraform/modules/docker/main.tf
+++ terraform/modules/docker/main.tf
@@ -1,15 +1,8 @@
 # The access token was created manually in Docker Hub, not managed by Terraform
 
-resource "docker_hub_repository" "postrix" {
-  name        = "postrix"
-  namespace   = "yuvadius"
-  description = "Postrix Docker repository"
-  private     = false
-}
-
-resource "docker_hub_repository" "postrix-dev" {
-  name        = "postrix-dev"
-  namespace   = "yuvadius"
-  description = "Postrix Development Docker repository"
+resource "docker_hub_repository" "core" {
+  namespace   = var.namespace
+  name        = "core"
+  description = "Core docker repository"
   private     = false
 }
