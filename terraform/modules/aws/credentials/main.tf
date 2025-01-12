3792a359e0f282fface86a06de439db89b0915fb
yuvalglizerin-eb <116079286+yuvalglizerin-eb@users.noreply.github.com>
2025-01-12 16:40:30 +0200

    Authenticate to aws via OIDC

--- terraform/modules/aws/credentials/main.tf
+++ terraform/modules/aws/credentials/main.tf
@@ -11,3 +11,42 @@ resource "aws_iam_access_key" "postrix" {
   user    = aws_iam_user.postrix.name
   status  = "Active"
 }
+
+# IAM Role for GitHub Actions
+resource "aws_iam_role" "github_actions" {
+  name = "github-actions-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRoleWithWebIdentity"
+        Effect = "Allow"
+        Principal = {
+          Federated = "arn:aws:iam::384389382109:oidc-provider/token.actions.githubusercontent.com"
+        }
+        Condition = {
+          StringLike = {
+            "token.actions.githubusercontent.com:sub": "repo:YuvalGlizerin/Postrix:*"
+          }
+          StringEquals = {
+            "token.actions.githubusercontent.com:aud": "sts.amazonaws.com"
+          }
+        }
+      }
+    ]
+  })
+}
+
+# Attach AdministratorAccess policy to the GitHub Actions role
+resource "aws_iam_role_policy_attachment" "github_actions_admin" {
+  role       = aws_iam_role.github_actions.name
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+}
+
+# Create the OIDC provider for GitHub Actions
+resource "aws_iam_openid_connect_provider" "github_actions" {
+  url             = "https://token.actions.githubusercontent.com"
+  client_id_list  = ["sts.amazonaws.com"]
+  thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1"]
+}
