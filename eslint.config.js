ab76e399b673139c191bf9327246ac3c766674df
yuvalglizerin-eb <116079286+yuvalglizerin-eb@users.noreply.github.com>
2025-01-22 19:34:48 +0200

    some more eslint rules

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,8 @@
+import js from '@eslint/js';
 import eslint from '@typescript-eslint/eslint-plugin';
 import tsParser from '@typescript-eslint/parser';
+import importPlugin from 'eslint-plugin-import';
+import globals from 'globals';
 
 export default [
   {
@@ -9,19 +12,40 @@ export default [
       '.history/**',
       '.git/**',
       'coverage/**'
-    ],
-    files: ['**/*.ts', '**/*.tsx'],
+    ]
+  },
+  js.configs.recommended,
+  {
+    files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
     languageOptions: {
       parser: tsParser,
       ecmaVersion: 2022,
-      sourceType: 'module'
+      sourceType: 'module',
+      globals: {
+        ...globals.node,
+        ...globals.jest
+      }
     },
     plugins: {
-      '@typescript-eslint': eslint
+      '@typescript-eslint': eslint,
+      'import': importPlugin
     },
     rules: {
       'quotes': ['error', 'single'],
-      'semi': ['error', 'always']
+      'semi': ['error', 'always'],
+      'import/order': ['warn', {
+        'newlines-between': 'always'
+      }],
+      'no-debugger': 'error',
+      'no-extra-semi': 'error',
+      'no-var': 'error',
+      'prefer-const': 'error',
+      'prefer-template': 'error',
+      'camelcase': ['error', {
+        'properties': 'never'
+      }],
+      'curly': ['error', 'all'],
+      'yoda': 'error'
     }
   }
 ]; 
