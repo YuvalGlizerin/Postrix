d8f00f1544f7de10a8fe3490d6a3d15e834c36e3
YuvalGlizerin <116079286+yuvalglizerin-eb@users.noreply.github.com>
2025-05-07 15:59:59 +0300

    Add namespace via template

--- packages/cannon-db/src/index.ts
+++ packages/cannon-db/src/index.ts
@@ -5,8 +5,7 @@ import secrets from 'secret-manager';
 import { PrismaClient } from './generated/prisma/client.ts';
 
 const databaseUrl = `postgresql://${secrets.POSTGRES_USERNAME}:${secrets.POSTGRES_PASSWORD}@${secrets.POSTGRES_HOST}:${secrets.POSTGRES_PORT}/${process.env.DATABASE}`;
-const schema = process.env.ENV === 'local' ? os.hostname() : 'postgres-prisma-kickoff';
-
+const schema = process.env.ENV === 'local' ? os.hostname() : process.env.NAMESPACE;
 const fullDatabaseUrl = `${databaseUrl}?schema=${schema}`;
 
 const prisma = new PrismaClient({
