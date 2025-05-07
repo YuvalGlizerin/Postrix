1241f22a58008a09bcb7547c6580a95af9beeab3
YuvalGlizerin <116079286+yuvalglizerin-eb@users.noreply.github.com>
2025-05-07 15:52:15 +0300

    Try again

--- packages/cannon-db/src/index.ts
+++ packages/cannon-db/src/index.ts
@@ -6,6 +6,7 @@ import { PrismaClient } from './generated/prisma/client.ts';
 
 const databaseUrl = `postgresql://${secrets.POSTGRES_USERNAME}:${secrets.POSTGRES_PASSWORD}@${secrets.POSTGRES_HOST}:${secrets.POSTGRES_PORT}/${process.env.DATABASE}`;
 const schema = process.env.ENV === 'local' ? os.hostname() : 'postgres-prisma-kickoff';
+
 const fullDatabaseUrl = `${databaseUrl}?schema=${schema}`;
 
 const prisma = new PrismaClient({
