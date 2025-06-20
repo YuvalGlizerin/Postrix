0471a904f52b3a0657bafe83da70e5551d418bd5
YuvalGlizerin <yuval.glizerin@postrix.io>
2025-06-21 00:37:03 +0300

    No external dependencies eslint

--- eslint.config.js
+++ eslint.config.js
@@ -72,6 +72,7 @@ export default [
     rules: {
       ...eslint.configs['recommended'].rules,
       'import/order': ['warn', { 'newlines-between': 'always' }],
+      'import/no-extraneous-dependencies': 'error',
       'no-console': 'warn',
       'no-debugger': 'error',
       'no-var': 'error',
